6698299c067c000511f8dc5a3390e6453dbbc653
DeeptiChincholi <deeptichincholi100@gmail.com>
2026-03-23 15:28:22 +0530

    Login User

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -32,4 +33,37 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // create JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      "mysecret123",   // later we can move to env
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+});
 module.exports = router;
